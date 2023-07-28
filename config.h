9ffa868d8c5d26be3d00a99ade6b6733de839235
Leonardo Lago <leonardog_rs@yahoo.com.br>
2023-07-27 22:46:41 -0300

    changing scripts path

--- config.h
+++ config.h
@@ -1,27 +1,35 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "~/Programming/Suckless/dwmblocks/scripts/music",               				    1,  	0},
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    {"", "sb-music", 1, 0},
 
-	{"",	"~/Programming/Suckless/dwmblocks/scripts/volume",									0,		10},
+    {"", "sb-volume", 0, 10},
+    
+    // {"", "sb-btc", 600, 5},
 
-/*    {" ",    "~/Programming/Suckless/dwmblocks/scripts/price xmr \"Monero\" 🔒",           	1200,   10}, */
+    // {"", "sb-xrd", 600, 5},
 
-    {"",    "~/Programming/Suckless/dwmblocks/scripts/vpn",     		       					0,      10},
+    // {"", "sb-paxg", 600, 5},
 
-    {"",    "setxkbmap -print | awk -F'+' '/xkb_symbols/ {print $2}'",     					 	0,  	10},
+    // {"", "sb-vpn", 0, 10},
 
-	{"",	"~/Programming/Suckless/dwmblocks/scripts/forecast",								1200,	10},
+    {"📱 ", "sb-phone-battery", 5, 5},
 
-    {"",    "netspeed",                                                         				5,      0},
+    // {"", "setxkbmap -print | awk -F'+' '/xkb_symbols/ {print $2}'", 0, 10},
 
-    {"CPU: "    , "~/Programming/Suckless/dwmblocks/scripts/cpu_usage",							5,      0},
+    // {"", "sb-forecast", 1200, 10},
 
-	{"RAM: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",						5,		0},
+    // {"", "netspeed", 5, 0},
 
-	{"", "date '+%a %b %d %H:%M:%S'",															1,		0},
+    {"CPU: ", "sb-cpu_usage", 5, 0},
+
+    {"RAM: ",  "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | " "sed s/i//g", 5, 0},
+
+    {"", "date '+%a %b %d %H:%M:%S'", 1, 0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 10;
